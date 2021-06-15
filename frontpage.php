6d24658eba3e321c52e634b503effe5a8b4069bb
Adrian Garner <acgarner11@yahoo.com>
2021-06-15 12:20:23 -0400

    :rocket: Added header and changed title text.

--- frontpage.php
+++ frontpage.php
@@ -13,18 +13,22 @@
 
 get_header(); ?>
 
-    <video src="https://foothillscollective.com/wp-content/uploads/2021/04/Res-Power-Background.mp4" autoplay loop playsinline muted></video>
+        <div class="bg-no-repeat bg-scroll bg-cover relative" style="background: linear-gradient(
+      rgba(0, 0, 0, 0.20),
+      rgba(0, 0, 0, 0.20)
+    ), url(https://images.unsplash.com/photo-1613206485381-b028e578e791?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2850&q=80);
+     background-position: top left;
+     height: 60vh;
+     ">
 
-    <div class="viewport-header">
-        <div class="head-container">
-            <div class="center add-padding">
-                <h1>Header Title</h1>
+            <div class="content-middle text-white text-center">
+                <h1 class="text-5xl mb-5">~ Happy Father's Day ~</h1>
+                <h1 class="text-3xl mb-5">~ From Foothills Church ~</h1>
+
             </div>
-            <hr style="width: 50%;">
-            <h2 class="center lr-padding no-margin dark-color-invert">Title</h2>
-            <h3 class="center lr-padding no-margin dark-color-invert">Subtitle</h3>
         </div>
-    </div>
+
+
 
 
 <?php
