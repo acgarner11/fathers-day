97273ea73fcfd145bd5461bb111bf27665922a19
Adrian Garner <acgarner11@yahoo.com>
2021-06-15 12:31:58 -0400

    :hammer: Fixed header on mobile.

--- frontpage.php
+++ frontpage.php
@@ -13,7 +13,7 @@
 
 get_header(); ?>
 
-        <div class="bg-no-repeat bg-scroll bg-cover relative" style="background: linear-gradient(
+    <div class="bg-no-repeat bg-scroll bg-cover relative" style="background: linear-gradient(
       rgba(0, 0, 0, 0.20),
       rgba(0, 0, 0, 0.20)
     ), url(https://images.unsplash.com/photo-1613206485381-b028e578e791?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2850&q=80);
@@ -21,14 +21,16 @@ get_header(); ?>
      height: 60vh;
      ">
 
-            <div class="content-middle text-white text-center">
-                <h1 class="text-5xl mb-5">~ Happy Father's Day ~</h1>
-                <h1 class="text-3xl mb-5">~ From Foothills Church ~</h1>
+        <div class="content-middle text-white text-center">
+            <h1 class="text-6xl mb-5"><span class="hidden md:inline">~</span> Happy Father's Day <span class="hidden md:inline">~</span></h1>
+            <h1 class="text-2xl mb-5"><span class="hidden md:inline">~</span> From Foothills Church <span class="hidden md:inline">~</span></h1>
 
-            </div>
         </div>
+    </div>
+    <div>
+        <p class="text-center">THANK YOU FOR CELEBRATING MOTHER'S DAY WITH US!</p>
 
-
+    </div>
 
 
 <?php
